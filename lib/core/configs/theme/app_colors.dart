ec73134cff72f2aa6f5bd56a9eee26fd3cf0b41b
Amal Pradeep <amalpradeep271@gmail.com>
2025-01-04 22:29:48 +0530

    Initial commit

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xffe21221);
+  static const background = Color(0xff181a20);
+  static const secondBackgound = Color(0xff35383f);
+  static const whiteColor = Color(0xFFFFFFFF);
+  static const hintColor = Color(0xffa7a7a7);
+}
